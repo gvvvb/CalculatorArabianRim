4d6faf6832e5d4edacc2c1152b3fcc75d6045b9e
KKartavbly <131470931+gvvvb@users.noreply.github.com>
2023-10-06 14:09:25 +0500

    Roman.java

--- CalculatorArabianRim/Roman.java
+++ CalculatorArabianRim/Roman.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Roman {
+
     private static final Map<Character, Integer> romanToArabic = new HashMap<>();
 
     static {
@@ -39,4 +40,27 @@ public class Roman {
         int arabicNumber = convert(romanNumber);
         System.out.println("Римское число " + romanNumber + " равно " + arabicNumber + " в арабской нотации.");
     }
+
+    public static String toRoman(int number) {
+        if (number < 1 || number > 10) {
+            throw new IllegalArgumentException("Число должно быть от 1 до 10.");
+        }
+
+        String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] arabicValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < romanNumerals.length; i++) {
+            while (number >= arabicValues[i]) {
+                result.append(romanNumerals[i]);
+                number -= arabicValues[i];
+            }
+        }
+
+        return result.toString();
+    }
+
+
+
 }
